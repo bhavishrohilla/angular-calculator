c5951975be8e8a4ac294c83cdbc7e55c38976853
Navneet Kumar <navneetkumar@Navneets-MacBook-Air.local>
2021-05-15 21:38:39 +0530

    fixed

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -7,53 +7,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
-  constructor() {
-    currentNumber: '0';
-    firstOperand: null;
-    operator: null;
-    waitForSecondNumber: false;
-   }
+  public currentNumber = '0';
+  public firstOperand :any= null;
+  public operator :any= null;
+  public waitForSecondNumber = false;
 
-  ngOnInit(): void {
-    public getNumber(v: string){
-      console.log(v);
-      if(this.waitForSecondNumber){
-        this.currentNumber = v;
-        this.waitForSecondNumber = false;
-      }else{
-        this.currentNumber === '0'? this.currentNumber =v: this.currentNumber +=v;
-      }
-    }
-    getDecimal(){
-      if(!this.currentNumber.includes('.')){
-        this.currentNumber += '.';
-      }
-    }
-    private doCalculation(op , secondOp){
-      switch (op){
-        case '+':
-          return this.firstOperand += secondOp;
-        case '-':
-          return this.firstOperand -=secondOp;
-        case '*':
-          return this.firstOperand *=secondOp;
-        case '/':
-          return this.firstOperand /=secondOp;
-        case '=':
-          return secondOp;
-      }
-    }
-    public getOperation(op: string){
-      console.log(op);
+  constructor() { }
 
-      if(this.firstOperand ===null){
-        this.firstOperand = Number(this.currentNumber);
+  ngOnInit() {
+  }
 
-      }else if(this.operator){
-        const result = this.docalculation(this.operator,Number(this.currentNumber))
-        this.currentNumber = String (result)
-        this.firstOperand = result;
-      }
-      this.operator = op;
+  public getNumber(v: string){
+    console.log(v);
+    if(this.waitForSecondNumber)
+    {
+      this.currentNumber = v;
+      this.waitForSecondNumber = false;
+    }else{
+      this.currentNumber === '0'? this.currentNumber = v: this.currentNumber += v;
+
     }
   }
+
+  getDecimal(){
+    if(!this.currentNumber.includes('.')){
+        this.currentNumber += '.'; 
+    }
+  }
+
+  private doCalculation(op:any , secondOp:any){
+    switch (op){
+      case '+':
+      return this.firstOperand += secondOp; 
+      case '-': 
+      return this.firstOperand -= secondOp; 
+      case '*': 
+      return this.firstOperand *= secondOp; 
+      case '/': 
+      return this.firstOperand /= secondOp; 
+      case '=':
+      return secondOp;
+    }
+  }
+  public getOperation(op: string){
+    console.log(op);
+
+    if(this.firstOperand === null){
+      this.firstOperand = Number(this.currentNumber);
+
+    }else if(this.operator){
+      const result = this.doCalculation(this.operator , Number(this.currentNumber))
+      this.currentNumber = String(result);
+      this.firstOperand = result;
+    }
+    this.operator = op;
+    this.waitForSecondNumber = true;
+
+    console.log(this.firstOperand);
+ 
+  }
+
+  public clear(){
+    this.currentNumber = '0';
+    this.firstOperand = null;
+    this.operator = null;
+    this.waitForSecondNumber = false;
+  }
+}
